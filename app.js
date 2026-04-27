05a18abcd7c1ab147eac73dd9a037246d1e2c8ef
Congtrinhh <trinhquycong@gmail.com>
2026-04-27 14:18:18 +0700

    check app built in prod or not

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const urlsRouter = require("./routes/urls");
 const logger = require("./config/logger");
 
 logger.info("logger: App.js loaded");
-console.log("console log: App.js loaded");
+console.log("console log: App.js loaded[updated]");
 
 const tracingMiddleware = require("./middleware/tracing");
 const requestLogger = require("./middleware/requestLogger");
